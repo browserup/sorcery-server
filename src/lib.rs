ac22698b85d55d2a397265ff815abbee357e0013
Eric Beland <ebeland@gmail.com>
2025-12-06 20:22:00 -0500

    Add core server foundation

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod parsing;
+pub mod routes;
+pub mod subdomain;
+pub mod tenant;
+
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub tenant_manager: Arc<tenant::TenantManager>,
+    pub base_domain: String,
+}
